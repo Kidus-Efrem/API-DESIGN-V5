81140b9738e008f417b70f940e5cf4119b741484
kidus <kidusefrem2@gmail.com>
2026-03-20 14:00:29 +0300

    Autosave(api-design-node-v5:src): updated server.ts @ 14:00

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,8 @@ const app = express()
 app.get('/health', (req, res) => {
 	res.send('<button>click</click>')
 })
+app.post('/cake', (req, res)=>{
+	res.send("<h1>here is your cake</h1>")
+})
 export {app}
 export default app
